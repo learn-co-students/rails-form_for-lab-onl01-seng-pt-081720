cd79867780742659e976087527bd5f2a0749d7aa
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-01-06 17:26:10 +0200

    Done.

---
+++ app/controllers/school_class.rb
@@ -0,0 +1,26 @@
+class SchoolClass < ApplicationController
+    
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+    
+    def update
+
+    end
+end
